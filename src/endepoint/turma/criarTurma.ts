a7af14da975bdcd343cd0de45eaf406cd35dc5f5
kill <wkdev7@gmail.com>
2022-06-23 19:46:33 -0300

    endpoint cruar turma feito

--- src/endepoint/turma/criarTurma.ts
+++ src/endepoint/turma/criarTurma.ts
@@ -4,8 +4,16 @@ import connection from '../../connection';
 export default async function criarTurma(req: Request, res: Response): Promise<void> {
 try {
     
-    const { nome, descricao, dataInicio, dataFim, professorId } = req.body;
+    const { name, estudantes, modulo, dataInicio, dataTermino, docentes } = req.body;
+    
 
+    if (!name || !estudantes || !modulo || !dataInicio || !dataTermino || !docentes) {
+        throw new Error('Dados insuficientes');
+    }
+
+    await connection("turma").insert({name, estudantes, modulo, dataInicio, dataTermino, docentes})
+
+    res.status(201).send({message: "Turma criada com sucesso!"});
 } catch (error: any) {
     res.status(500).json({menssage: error.message});
     
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
-import express, {Express} from "express"
-import cors from "cors"
+
 import criarTurma from "./endepoint/turma/criarTurma"
-import pegarTurma from "./endepoint/turma/pegarTurmas"
+// import pegarTurma from "./endepoint/turma/pegarTurmas"
+import app from "./app"
 
 
-const app: Express = express()
-app.use(express.json())
-app.use(cors())
+
 
 app.post("/turma", criarTurma)
-app.get("/turma", pegarTurma)
+// app.get("/turma", pegarTurma)

9a0bbe5cb0f6f1808ec33fff6bd9a5c9a8c5b8f6
kill <wkdev7@gmail.com>
2022-06-24 16:39:30 -0300

    endpoints de criar e pegar funcionando

---
+++ src/endepoint/estudantes/criarEstudante.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import connection from '../../connection';
+
+
+export default async function criarAluno(req: Request, res: Response): Promise<void> {
+    try {
+        const { name, email, datanascimento, hobbies} = req.body;
+        if ( !name || !email || !datanascimento || !hobbies) {
+            throw new Error('Dados insuficientes');
+        }
+        await connection("estudante").insert({ name, email, datanascimento, hobbies })
+        res.status(201).json({message: "Aluno cadastrado com sucesso!"});
+    } catch (error: any) {
+        res.status(500).json({menssage: error.message});
+    }
+    }
+
--- src/endepoint/turma/pegarTurmas.ts
+++ src/endepoint/turma/pegarTurmas.ts
@@ -3,6 +3,8 @@ import connection from '../../connection';
 
 export default async function pegarTurma(req: Request, res: Response): Promise<void> {
 try {
+    const allTurmas = await connection("turma").select("*");
+    res.status(200).json(allTurmas);
     
 } catch (error: any) {
     res.status(500).json({menssage: error.message});
